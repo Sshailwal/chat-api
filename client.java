6445d9cf4ef55d3ec3ef684980dc1e07f7d5f1d1
Sshailwal <shubhratshailwal8@gmail.com>
2024-08-14 14:05:33 +0530

    basic  error changes

--- client.java
+++ client.java
@@ -20,40 +20,49 @@ public class client {
         } catch (Exception e) {
             e.printStackTrace();
         }
+            
+        
     }
     public void startReading(){
         Runnable r1=()->{
           System.out.println("reading started...");
-          while(true ){
-             try {
-              String msg;
-              msg = br.readLine();
-             if(msg.equals("exit")){
-                 System.out.println("Server terminated the chat");
-                 break;
-             }
-             System.out.println("Server:" +msg);
-             } catch (Exception e) {
-              e.printStackTrace();
-             }
+          try {
+            while(true ){ 
+                String msg;
+                msg = br.readLine();
+               if(msg.equals("exit")){
+                   System.out.println("Server terminated the chat");
+                   socket.close();
+                     
+                   break;
+               }
+               System.out.println("Server:" +msg);
+               }
+          
+          } catch (Exception e) {
+            e.printStackTrace();
           }
-        
-      };
+        };
       new Thread(r1).start();
       }
       public void startWriting(){
           Runnable r2=()->{
               System.out.println("writer stsarted..");
-            while(true){
               try {
-                  BufferedReader br1 = new BufferedReader(new InputStreamReader(System.in));
-                  String content = br1.readLine();
-                  out.println(content);
-                  out.flush();
+                while(true){
+                    BufferedReader br1 = new BufferedReader(new InputStreamReader(System.in));
+                     String content = br1.readLine();
+                    out.println(content);
+                    out.flush();
+                    if (content.equals("exit")){
+                        socket.close();
+                        break;
+                    }
+                }
               } catch (Exception e) {
-                  e.printStackTrace();
-              }
+            e.printStackTrace();  
             }
+            
           };
           new Thread(r2).start();
       }
